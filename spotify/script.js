fc52d63758555f22c829555dafe02b27f69ed461
zero0O2 <148266981+zero0O2@users.noreply.github.com>
2025-02-09 12:57:24 -0300

    artists

--- spotify/script.js
+++ spotify/script.js
@@ -19,79 +19,124 @@ const searchInput = document.getElementById('searchInput')
 const resultPlaylist = document.getElementById('result')
 const divArtists = document.getElementById('artists')
 const resultArtists = document.getElementById('artistsSection')
+const artistsPlay = document.getElementById('artistsPlay')
 const playlistSection = document.getElementById('playlistSection')
+const headerNavigation = document.getElementById('headerNavigation')
+const footer = document.querySelector('.footer')
 
 const api = `api-artists/artists.json`;
 
+
+
+
+function seguirButton(buttonSeguir){
+    if(!window.localStorage.getItem('artist')){
+        window.localStorage.setItem('artist','Seguir')
+        buttonSeguir.innerHTML = window.localStorage.getItem('artist')
+    }else{
+        buttonSeguir.innerHTML = window.localStorage.getItem('artist')
+    }
+    buttonSeguir.addEventListener('click' , function(){
+        if(buttonSeguir.textContent == 'Seguir'){
+            buttonSeguir.innerHTML = 'Seguindo'
+            window.localStorage.setItem('artist','Seguindo')
+        } else {
+            buttonSeguir.innerHTML = 'Seguir'
+            window.localStorage.setItem('artist','Seguir')
+        }
+    })
+}
+
 //                          Funções     
 
-function animationPlay(artistsCard,play){
+
+
+function artistsForEach(artistsCard,play,nome,api){
     artistsCard.forEach((element,index)=>{
+        // ANIMAÇÃO
         element.addEventListener('mouseover',function(){
             play[index].classList.remove('hidden')
         })
         element.addEventListener('mouseout',function(){
             play[index].classList.add('hidden')
         })
+        // DISPLAY DE ARTISTA
+        element.addEventListener('click',function(){
+            resultArtists.classList.add('hidden')
+            footer.classList.add('hidden')
+            artistsPlay.classList.remove('hidden')
+            searchInput.value = ''
+            artistsPlay.innerHTML = `
+            <div class='artistAside' id='background'>
+                <span class="title" id="titleArtist">${nome[index].textContent}</span>
+            </div>
+            <div class="artistMusic">
+                <div>
+                    <button class="fa-solid fa-play"></button>
+                    <button id="seguir"></button>
+                    <button id="mais">
+                        <p>. . .</p>
+                    </button>
+                </div>
+            </div>`
+            let buttonSeguir = document.getElementById('seguir')
+            seguirButton(buttonSeguir)
+            let background = document.getElementById('background')
+            background.style.backgroundImage = `none`
+            background.style.backgroundImage = `url('${api[index].banner}')`
+        })
     })
-}
-
-function artistsPage(name){
-    console.log(name)
 }
 
 
 function artistsDisplay(resultArtists , searchTerm){
     resultArtists.innerHTML = ''
     fetch(api)
-        .then((response) => response.json())
-        .then((result) => result.artists)
-        .then((api) => {
-            api.forEach((element,index) => {
-                let filter = element.name.toLowerCase()
-                let string = searchTerm
-                let name = element.name
-                if(filter.indexOf(string) != -1){
-
-                    resultArtists.innerHTML += `
-                    <a href="" class="artistsCard" id="artistsCard-${index}">
+    .then((response) => response.json())
+    .then((result) => result.artists)
+    .then((api) => {
+        api.forEach((element,index) => {
+            let filter = element.name.toLowerCase()
+            let string = searchTerm
+            let name = element.name
+            if(filter.indexOf(string) != -1){
+                
+                resultArtists.innerHTML += `
+                    <a class="artistsCard" id="artistsCard-${index}">
                         <img class="artistsFoto" id="artistsFoto" src="${element.urlImg}">
                         <span class="hidden play" id="play-${index}">
-                           <i class="fa-solid fa-play"></i>
+                            <i class="fa-solid fa-play"></i>
                         </span>
                         <div class="artistsContent">
                             <span class="artistsName" id="name-${index}">${element.name}</span>
                             <span class="indentifier" id="indentifier">${element.genre}</span>
                         </div>
                     </a>`                    
-                }
-            })
+            }
+        })
+        const artistsCard = document.querySelectorAll(`.artistsCard`)
+        const play = document.querySelectorAll('.play')
+        const nome = document.querySelectorAll('.artistsName')
+        artistsForEach(artistsCard,play,nome,api)
 
-            const artistsCard = document.querySelectorAll(`.artistsCard`)
-            const play = document.querySelectorAll('.play')
-            const name = document.querySelectorAll('.artistsName')
 
-            animationPlay(artistsCard,play)
-
-            artistsCard.forEach((element,index) => {
-                element.addEventListener('click',function(){
-                    console.log(name[index].textContent)
-                })
-            })
-
-        });
+    });
 }
+        
+        
+        document.addEventListener('input', function() {
+            const searchTerm = searchInput.value.toLowerCase()  
+            if(searchTerm === ''){
+                resultPlaylist.classList.remove('hidden')
+                divArtists.classList.add('hidden')
+            } else {
+                resultPlaylist.classList.add('hidden')
+                artistsPlay.classList.add('hidden')
+                divArtists.classList.remove('hidden')
+                resultArtists.classList.remove('hidden')
+                
+            }
+            artistsDisplay(resultArtists ,searchTerm)
+        })
 
-
-document.addEventListener('input', function() {
-    const searchTerm = searchInput.value.toLowerCase()  
-    if(searchTerm === ''){
-        resultPlaylist.classList.remove('hidden')
-        divArtists.classList.add('hidden')
-    } else {
-        resultPlaylist.classList.add('hidden')
-        divArtists.classList.remove('hidden')
-    }
-    artistsDisplay(resultArtists ,searchTerm)
-})
-
+        
